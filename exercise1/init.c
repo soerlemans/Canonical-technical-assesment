6fa2b127985bc1862e534d8556defe345343ba56
sappigeninja <sebasoerlemans@gmail.com>
2023-08-05 18:34:50 +0200

    Can create rootfs, download kernel, next up is extraction and compilation

---
+++ exercise1/init.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main() {
+  printf("Hello World!\n");
+  sleep(0xFFFFFFFF);
+
+  return 0;
+}
--- exercise1/qemu.sh
+++ exercise1/qemu.sh
@@ -1,16 +1,59 @@
 #!/usr/bin/env bash
 
+<<EOF
+This script assumes that you have qemu, gcc preinstalled.
+EOF
 
 # Change current working directory to location of the script
 cd "$(dirname "")"
 pwd -P
 
-readonly IMG="qemu.img"
+# Readonly variables:
+readonly TMP_DIR="${TMP:-tmp}"
 
-# Create the qemu-img
-qemu-img create -f qcow2 ${IMG} 40G
+readonly ROOTFS_DIR="${TMP_DIR}/rootfs"
+readonly ROOTFS="rootfs.cpio"
+readonly ROOTFS_PATH="${TMP_DIR}/${ROOTFS}"
 
+readonly KERNEL_VERSION="${KERNEL_VERSION:-6.4}"
+readonly KERNEL_URL="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-${KERNEL_VERSION}.tar.gz"
+readonly KERNEL_PATH="${TMP_DIR}/kernel.tar.gz"
+
+# Functions:
+
+# Start of script:
+mkdir -p "${TMP_DIR}"
+
+# Create root filesystem
+echo "Creating root filesystem ($ROOTFS)..."
+mkdir -p "${ROOTFS_DIR}"
+cc -static init.c -o "${ROOTFS_DIR}/init"
+
+# Use a subshell to cleanly enter
+(
+	cd "${TMP_DIR}/rootfs"
+
+	# Copy files in the rootfs folder to an archive and compress it
+	find | cpio --quiet --create --format=newc | bzip2 --stdout >../$ROOTFS
+)
+echo
+
+echo "Downloading Linux kernel ($KERNEL_VERSION)..."
+wget --output-document="$KERNEL_PATH" \
+	--quiet \
+	--show-progress \
+	"$KERNEL_URL"
+echo
+
+echo "Compile kernel..."
+(
+	cd "$TMP_DIR"
+	tar -xzvf "$KERNEL_PATH"
+
+	cd 
+)
+
+echo "Start QEMU..."
 qemu-system-x86_64 \
-    -enable-kvm    \
-		-m size=4G
-    $IMG
+	-kernel "${KERNEL_PATH}" \
+	-initrd "${ROOTFS_PATH}"
