77ba48e74e1373e373eae1750456020d45949a4d
sappigeninja <sebasoerlemans@gmail.com>
2023-08-06 14:13:44 +0200

    Finished exercise 1 starting exercise 2

--- exercise1/qemu.sh
+++ exercise1/qemu.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 <<EOF
-This script assumes that you have qemu, gcc preinstalled.
+This script creates a root filesystem with an init binary.
+It then downloads and compiles a Linux kernel and then uses qemu to emulate hardware.
+
+This script assumes you have qemu already installed.
+We use cc as it is part of the POSIX standard.
+We prefer 'mkdir -p' over 'mkdir --parents' as it is considered more portable.
+We use the 6.4 release of the Linux kernel as it is the most recent and least likely to compile without issues.
 EOF
 
 # Change current working directory to location of the script
@@ -14,21 +20,22 @@ readonly TMP_DIR="${TMP:-tmp}"
 readonly ROOTFS_DIR="${TMP_DIR}/rootfs"
 readonly ROOTFS="rootfs.cpio"
 
+# 6.1 is the LTS variant
 readonly KERNEL_VERSION="6.4"
+readonly KERNEL_DIR="linux-$KERNEL_VERSION"
 
 # Functions:
 function rootfs {
-
 	# Create root filesystem
 	echo "Creating root filesystem ($ROOTFS)..."
-	mkdir -p "${ROOTFS_DIR}"
+	mkdir -p "$ROOTFS_DIR"
 
 	# Compile init program
 	cc -static init.c -o "${ROOTFS_DIR}/init"
 
 	# Use a subshell to cleanly enter
 	(
-		cd "${ROOTFS_DIR}"
+		cd "$ROOTFS_DIR"
 
 		# Copy files in the rootfs folder to an archive and compress it
 		find | cpio --quiet --create --format=newc | bzip2 --stdout >../$ROOTFS
@@ -37,54 +44,66 @@ function rootfs {
 
 # Download and compile a version of the linux kernel
 function kernel {
-	local TAR="${1:-kernel.tar.gz}"
-	local VERSION="${2:-$KERNEL_VERSION}"
-	local URL="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-${VERSION}.tar.gz"
-	local EXTRACTED="linux-$VERSION"
+	local tarball="${1:-linux-${KERNEL_VERSION}.tar.gz}"
+	local version="${2:-$KERNEL_VERSION}"
+	local url="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-${version}.tar.gz"
+
+	https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.43.tar.xz
+	local kernel_dir="linux-$version"
 
 	# Download kernel
-	if [[ ! -f "$TAR" ]]; then
+	if [[ ! -f "$tarball" ]]; then
 		echo "Downloading Linux kernel (version $VERSION)..."
 
-		wget --output-document="$TAR" \
+		wget --output-document="$tarball" \
 			--quiet \
 			--show-progress \
-			"$URL"
+			"$url"
 	fi
 
 	# Extract kernel
-	if [[ ! -d "$EXTRACTED" ]]; then
+	if [[ ! -d "$kernel_dir=" ]]; then
 		echo "Extracting kernel"
-		tar -xzf "$TAR"
+		tarball -xzf "$tarball"
 	fi
 
 	# Configure and compile kernel
 	(
-		cd "$EXTRACTED"
+		cd "$kernel_dir="
 
 		make defconfig # Use default config
-		# make -j
-		make
+		make -j 2      # Use two threads to compile
 	)
 }
 
 function qemu {
-	echo "Start QEMU..."
+	local bzimage="${1:-${KERNEL_DIR}/arch/x86_64/boot/bzImage}"
+	local rootfs= "${2:-$ROOTFS}"
+
+	echo "Starting QEMU..."
+
+	# Now pass the kernel binary and rootfs archive to qemu
 	qemu-system-x86_64 \
-		-kernel "$1" \
-		-initrd "${2:-$ROOTFS}"
+		-kernel "$bzimage" \
+		-initrd "$rootfs"
 }
 
 # Start of script:
+# Debugging flag
+if [[ -n "$DEBUG" ]]; then
+	set -x
+fi
+
 mkdir -p "$TMP_DIR"
 
 # Create rootfs
 rootfs
 
+# Continue working from tmp dir
 cd "$TMP_DIR"
 
 # Download, configure and compile kernel
-kernel "kernel.tar.gz"
+kernel
 
 # Run qemu
-qemu "kernel.tar.gz"
+qemu
