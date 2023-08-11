5e2b0f88a50f0ba6dfebdca244ce51059b072719
sappigeninja <sebasoerlemans@gmail.com>
2023-08-11 14:44:29 +0200

    Removed setupSuite function

--- exercise2/main_test.go
+++ exercise2/main_test.go
@@ -6,15 +6,6 @@ import (
 	"testing"
 )
 
-func setupTest(t *testing.T) string {
-	path, err := copy_template()
-	if err != nil {
-		t.Errorf("Failed to copy template for Test: %q", err)
-	}
-
-	return path
-}
-
 func TestNonExistentFile(t *testing.T) {
 	err := Shred("non_existent_file.txt")
 	if !errors.Is(err, fs.ErrNotExist) {
